f582e787effc7fa9ecf145d82dc564c1d8bcea89
MarcosPauloDeveloper <marcospaulodeveloper@gmail.com>
2022-09-20 21:18:38 -0300

    register

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('valida_cadastro/', views.validate_registration, name='validate_registration'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
+from django.shortcuts import redirect
+from hashlib import sha256
 
 
 def login(request):
@@ -7,4 +10,25 @@ def login(request):
 
 
 def register(request):
-    return render(request, 'register.html')
+    status = request.GET.get('status')
+    return render(request, 'register.html', {'status': status})
+
+
+def validate_registration(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    if len(name.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/auth/login/?status=1')
+
+    user = User.objects.filter(email=email)
+    if len(user):
+        return redirect('/auth/login?status=2')
+    try:
+        password = sha256(password.encode()).hexdigest()
+        user = User(name=name, password=password, email=email)
+        user.save()
+        return redirect('/auth/login/?status=0')
+    except ValueError:
+        return redirect('/auth/login/?status=3')
